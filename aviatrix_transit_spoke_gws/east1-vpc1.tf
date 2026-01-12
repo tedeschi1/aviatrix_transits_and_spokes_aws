7fd998b9e1b2e9f230aeca0aaa6e60820e9a6c21
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-12 14:15:25 -0600

    Update east1-vpc1.tf

--- aviatrix_transit_spoke_gws/east1-vpc1.tf
+++ aviatrix_transit_spoke_gws/east1-vpc1.tf
@@ -7,5 +7,7 @@ module "aws_spoke_vpc1_us-east-1" {
   cidr            = "10.1.0.0/20"
   region          = "us-east-1"
   account         = "aws_admin"
-  #transit_gw      = "avx-eu-west-1-transit"
+  #transit_gw     = "avx-eu-west-1-transit"
+  attached        = false
+  attached_gw_egress = false
 }
