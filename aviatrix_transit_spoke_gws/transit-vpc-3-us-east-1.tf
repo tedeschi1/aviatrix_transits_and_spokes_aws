852d050ca05530cde7f1fe513d228aa1d9f9b9b5
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-12 16:22:37 -0600

    deploy aviatrix transit

--- aviatrix_transit_spoke_gws/transit-vpc-3-us-east-1.tf
+++ aviatrix_transit_spoke_gws/transit-vpc-3-us-east-1.tf
@@ -1,6 +1,7 @@
 module "aws_transit_vpc3_us-east-1" {
   source          = "terraform-aviatrix-modules/mc-transit/aviatrix"
   version         = "8.2.0"
+  cloud           = "AWS"
   region          = "us-east-1"
   name            = "transit-vpc3-us-east-1"
   account         = "aws_admin"
