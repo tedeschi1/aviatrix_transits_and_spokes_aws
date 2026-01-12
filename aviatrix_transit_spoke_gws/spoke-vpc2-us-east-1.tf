f48b88171a4880a933b2457e5756780679a5133b
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-12 16:00:21 -0600

    aviatrix vpc deployment

--- aviatrix_transit_spoke_gws/spoke-vpc2-us-east-1.tf
+++ aviatrix_transit_spoke_gws/spoke-vpc2-us-east-1.tf
@@ -8,6 +8,7 @@ module "aws_spoke_vpc2_us-east-1" {
   region          = "us-east-1"
   account         = "aws_admin"
   #transit_gw     = "avx-eu-west-1-transit"
+  use_existing_vpc = true
   vpc_id          = "vpc-0a8f6238a5af3f811"
   gw_subnet       = "10.1.22.0/24"
   hagw_subnet     = "10.1.23.0/24"
