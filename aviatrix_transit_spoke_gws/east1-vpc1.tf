a07a492deb422217849293548cb934a2f8232e84
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-12 14:10:12 -0600

    Update east1-vpc1.tf

--- aviatrix_transit_spoke_gws/east1-vpc1.tf
+++ aviatrix_transit_spoke_gws/east1-vpc1.tf
@@ -1,4 +1,11 @@
-module "mc-spoke" {
+module "aws_spoke_vpc1_us-east-1" {
   source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
   version = "8.2.0"
+
+  cloud           = "AWS"
+  name            = "spoke_vpc1_us-east-1"
+  cidr            = "10.1.0.0/20"
+  region          = "us-east-1"
+  account         = "aws_admin"
+  #transit_gw      = "avx-eu-west-1-transit"
 }
